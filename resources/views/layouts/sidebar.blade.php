ab49838625d74dc9adfacbc2afb9d4fd356f1389
abdelmageddMohamedd <abdelmagedd@blue-developments.team>
2025-07-25 20:16:35 +0200

    Add Hotel Addon Fields link to sidebar navigation

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -107,6 +107,16 @@
                     </li>
                 @endif
 
+                {{-- Hotel Addon Fields --}}
+                @if (has_permissions('read', 'hotel_addon_field'))
+                    <li class="sidebar-item">
+                        <a href="{{ route('hotel-addon-field.index') }}" class='sidebar-link'>
+                            <i class="bi bi-plus-square"></i>
+                            <span class="menu-item">{{ __('Hotel Addon Fields') }}</span>
+                        </a>
+                    </li>
+                @endif
+
                 {{-- Hotel Room Types --}}
                 @if (has_permissions('read', 'hotel_room_types'))
                     <li class="sidebar-item">
