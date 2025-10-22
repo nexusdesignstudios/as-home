96a7ea27d9eb5ae8a687634eeeeeaaa763c96daa
Mahammed Abdelfattah <mahammed8067@gmail.com>
2025-10-23 02:41:01 +0300

    dsdmmmmmnlklhhks

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9186,6 +9186,7 @@ class ApiController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'property_id' => 'required|exists:propertys,id',
+            'customer_id' => 'required|exists:customers,id',
             'customer_name' => 'required|string|max:255',
             'customer_phone' => 'required|string|max:20',
             'customer_email' => 'required|email|max:255',
@@ -9251,7 +9252,7 @@ class ApiController extends Controller
 
             // Create reservation record for the revenue tab
             $reservationData = [
-                'customer_id' => $property->user_id, // Property owner ID
+                'customer_id' => $request->customer_id, // Customer making the reservation
                 'customer_name' => $request->customer_name,
                 'customer_phone' => $request->customer_phone,
                 'customer_email' => $request->customer_email,
