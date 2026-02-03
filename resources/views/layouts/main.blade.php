05fecf9ada9ef686366b5efd8a994a70f8cdca54
mahammed80 <mahammed8067@gmail.com>
2026-02-03 14:46:32 +0200

    google analytics

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,6 +11,15 @@
 @endif
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17844407850"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'AW-17844407850');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
